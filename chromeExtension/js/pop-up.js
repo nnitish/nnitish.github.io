73991e30d05880e973fc9adc681e83fe39ab9b1d
Nitish Kumar <nitish.kumar@mobtexting.com>
2017-12-30 11:54:41 +0530

    by clicking on extension hello-world will be printed

---
+++ chromeExtension/js/pop-up.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+	
+	var checkPageClick = document.getElementById('checkPage');
+
+	checkPageClick.addEventListener("click", function(){
+		alert('Hello World');
+	});
+});
